32af1d9af30a6f0af82542edbb7c3dceb5170423
ThePianoDentist <jbknight07@gmail.com>
2017-02-15 02:41:10 +0000

    Do non-casting method for insertion sort

--- src/sort.rs
+++ src/sort.rs
@@ -1,15 +1,20 @@
 pub fn debug_insertion_sort(arr: &mut [f64]) -> &mut [f64]{
     for x in 1..arr.len(){
         let value = arr[x];
-        let mut comparison_index = x as i64 - 1;
-        println!("Value: {:?}, Comparison Value: {:?}", value, arr[comparison_index as usize]);
-        while comparison_index >= 0 && arr[comparison_index as usize] > value{
-            arr[comparison_index as usize + 1] = arr[comparison_index as usize];
+        let mut comparison_index = x - 1;
+        println!("Value: {:?}, Comparison Value: {:?}", value, arr[comparison_index ]);
+        let mut offset = 0; // This avoids lots of casting
+        while arr[comparison_index] > value{
+            arr[comparison_index  + 1] = arr[comparison_index ];
             println!("Swap: {:?}", arr);
+            if comparison_index == 0{
+                offset += 1;
+                break
+            }
             comparison_index -= 1;
         }
-        arr[(comparison_index + 1) as usize] = value;
-        if comparison_index != x as i64 - 1{
+        arr[comparison_index + 1 - offset] = value;
+        if comparison_index != x - 1{
             println!("Swap back original: {:?}", arr);
         }
     }
