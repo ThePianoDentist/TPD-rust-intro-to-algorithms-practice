98152f1cc903c5956d6aaae68f192e5bcd30e4bd
ThePianoDentist <jbknight07@gmail.com>
2017-02-15 03:31:22 +0000

    Added macro for print/dont print

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 pub mod sort;
 use sort::*;
+
 fn main() {
     let mut input = [9.5, 7.4, 9.2, 2.3, 9.9, 4.2, 25.9, 13.2];
-    debug_insertion_sort(&mut input);
+    let print = false;
+    match print{
+        true => insertion_sort_debug(&mut input),
+        false => insertion_sort(&mut input)
+    }
     println!("{:?}", input);
 }
--- src/sort.rs
+++ src/sort.rs
@@ -1,24 +1,54 @@
-pub fn debug_insertion_sort(arr: &mut [f64]) -> &mut [f64]{
+macro_rules! insertion_sort{
+    (do_print) => 
+(pub fn insertion_sort_debug(arr: &mut [f64]){
     for x in 1..arr.len(){
         let value = arr[x];
         let mut comparison_index = x - 1;
         println!("Value: {:?}, Comparison Value: {:?}", value, arr[comparison_index ]);
-        let mut offset = 0; // This avoids lots of casting
-        while arr[comparison_index] > value{
-            arr[comparison_index  + 1] = arr[comparison_index ];
+        loop{
+            if arr[comparison_index] <= value{
+                arr[comparison_index + 1] = value;
+                println!("Swap back original: {:?}", arr);
+                break;
+            }
+            arr[comparison_index + 1] = arr[comparison_index];
             println!("Swap: {:?}", arr);
             if comparison_index == 0{
-                offset += 1;
+                arr[comparison_index] = value;
+                println!("Swap back original: {:?}", arr);
                 break
             }
             comparison_index -= 1;
         }
-        arr[comparison_index + 1 - offset] = value;
-        if comparison_index != x - 1{
-            println!("Swap back original: {:?}", arr);
-        }
     }
     println!("{:?}", arr);
-    return arr
+    return
+});
+(dont_print) => (
+    
+pub fn insertion_sort(arr: &mut [f64]){
+    for x in 1..arr.len(){
+        let value = arr[x];
+        let mut comparison_index = x - 1;
+        loop{
+            if arr[comparison_index] <= value{
+                arr[comparison_index + 1] = value;
+                break;
+            }
+            arr[comparison_index + 1] = arr[comparison_index];
+            if comparison_index == 0{
+                arr[comparison_index] = value;
+                break
+            }
+            comparison_index -= 1;
+        }
+    }
+    return
 }
 
+    )
+
+}
+
+insertion_sort!(do_print);
+insertion_sort!(dont_print);
